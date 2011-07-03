3941f386dddf255d9d51ce0e1afd5ee27bd52081
tinygrasshopper <jsnaik@gmail.com>
2011-07-04 00:53:30 +0530

    Added yasnippet and zencoding

--- init.el
+++ init.el
@@ -9,6 +9,15 @@
 (add-to-list 'ac-dictionary-directories "~/.emacs.d/packages/installed/auto-complete/ac-dict")
 (ac-config-default)
 
+(add-to-list 'load-path "~/.emacs.d/packages/zencoding")
+(require 'zencoding-mode)
+(add-hook 'sgml-mode-hook 'zencoding-mode)
+
+(add-to-list 'load-path "~/.emacs.d/packages/yasnippet")
+(require 'yasnippet)
+(yas/initialize)
+(yas/load-directory "~/.emacs.d/packages/yasnippet/snippets")
+
 (load-file "~/.emacs.d/custom.el")
 
 ;; Remove the menu and tool bar's
