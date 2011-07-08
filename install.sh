41e2485b650e977b3b1febfba65c9878aee56796
tinygrasshopper <jsnaik@gmail.com>
2011-07-08 22:09:37 +0530

    mkdir -p does the trick even if the directory exists

--- install.sh
+++ install.sh
@@ -1,7 +1,4 @@
-if [-d "tmp"]; then
-  mkdir tmp
-fi
-
+mkdir -p tmp
 
 wget http://download.savannah.gnu.org/releases/color-theme/color-theme-6.6.0.zip -O tmp/color-theme-6.6.0.zip
 unzip tmp/color-theme-6.6.0.zip -d packages/external/
