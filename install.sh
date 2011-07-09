473aa051c52118902dfe9c39828794f7f09e59cb
tinygrasshopper <jsnaik@gmail.com>
2011-07-09 21:35:57 +0530

    Added rinari

--- install.sh
+++ install.sh
@@ -1,5 +1,7 @@
 git submodule init
 git submodule update
 
+(cd packages/rinari && git submodule init && git submodule update)
+
 mkdir -p packages/auto-complete
 (cd src-packages/auto-complete/ && make && make install DIR=~/.emacs.d/packages/auto-complete)
