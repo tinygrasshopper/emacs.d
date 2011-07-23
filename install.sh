60084ee04b03a3077c64bd2d8c51c7c1031b767f
tinygrasshopper <jsnaik@gmail.com>
2011-07-23 11:38:52 +0530

    Made install idempotent

--- install.sh
+++ install.sh
@@ -3,5 +3,6 @@ git submodule update
 
 (cd packages/rinari && git submodule init && git submodule update)
 
+rm -rf ~/.emacs.d/packages/auto-complete
 mkdir -p packages/auto-complete
 (cd src-packages/auto-complete/ && make && make install DIR=~/.emacs.d/packages/auto-complete)
