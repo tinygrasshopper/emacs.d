f9f4df7b2d076d9ec13a0cc1394d8b6ba2eb649b
tinygrasshopper <jsnaik@gmail.com>
2011-08-03 16:17:08 +0530

    Added sass mode

---
+++ conf-packages/sass-mode.el
@@ -0,0 +1,2 @@
+(add-to-list 'load-path "~/.emacs.d/packages/sass-mode")
+(require 'sass-mode)
--- init.el
+++ init.el
@@ -15,6 +15,7 @@
 (load-package "wrap-region")
 (load-package "egg")
 (load-package "haml-mode")
+(load-package "sass-mode")
 
 ;; Snippet Directory
 (setq yas/my-directory '("~/.emacs.d/packages/yasnippet/snippets" "~/.emacs.d/packages/cucumber/snippets" "~/.emacs.d/packages/yasnippets-rails/rails-snippets"))
