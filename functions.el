430fe8e855c2c3900d24d7c1892625df38cf70cc
Ginette Vellera <ginettv@thoughtworks.com>
2011-08-25 19:15:10 +0530

    adding transparency for the windows

--- functions.el
+++ functions.el
@@ -86,3 +86,17 @@ With arg copies and reinserts last line."
   arg lines up."
    (interactive "*p")
    (move-text-internal (- arg)))
+
+;; function for transparency
+;;(set-frame-parameter (selected-frame) 'alpha '(<active> [<inactive>]))
+(set-frame-parameter (selected-frame) 'alpha '(85 50))
+(add-to-list 'default-frame-alist '(alpha 85 50))
+(eval-when-compile (require 'cl))
+(defun toggle-transparency ()
+  (interactive)
+  (if (/=
+       (cadr (frame-parameter nil 'alpha))
+       100)
+      (set-frame-parameter nil 'alpha '(85 85))
+    (set-frame-parameter nil 'alpha '(85 50))))
+
