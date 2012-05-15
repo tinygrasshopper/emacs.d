fbed4ecdba1b13268727d82827102aff5a9be29b
Jatin Naik <jsnaik@gmail.com>
2012-05-15 11:51:18 +0100

    Adding color-theme from savannah http downloads

--- install.sh
+++ install.sh
@@ -3,6 +3,9 @@ git submodule update
 
 (cd packages/rinari && git submodule init && git submodule update)
 
+curl -L0 http://download.savannah.gnu.org/releases/color-theme/color-theme-6.6.0.tar.gz | tar -xvz
+mv color-theme-6.6.0 packages/color-theme
+
 rm -rf ~/.emacs.d/packages/auto-complete
 mkdir -p packages/auto-complete
-(cd src-packages/auto-complete/ && make && make install DIR=~/.emacs.d/packages/auto-complete)
+(cd src-packages/auto-complete/ && make && make install DIR=~/.emacs.d/packages/auto-complete) > /dev/null
