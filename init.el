93ec0f9a429f659483e6b7e30c79a74dddc869c2
Jatin Naik <jsnaik@gmail.com>
2012-09-19 08:53:10 +0100

    Added package fetch if packages not installed

--- init.el
+++ init.el
@@ -2,3 +2,14 @@
 (add-to-list 'package-archives
 	     '("marmalade" . "http://marmalade-repo.org/packages/") t)
 (package-initialize)
+
+
+(when (not package-archive-contents)
+  (package-refresh-contents))
+
+(defvar my-packages '(js2-mode ruby-mode)
+  "A list of packages to ensure are installed at launch.")
+
+(dolist (p my-packages)
+  (when (not (package-installed-p p))
+    (package-install p)))
