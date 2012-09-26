beaed8e7da6ba5a65dd5505a918383c9077e0557
Jatin Naik <jsnaik@gmail.com>
2012-09-26 19:05:03 +0100

    All packages now fetching

--- init.el
+++ init.el
@@ -1,13 +1,29 @@
 (require 'package)
-(add-to-list 'package-archives
-	     '("marmalade" . "http://marmalade-repo.org/packages/") t)
+(add-to-list 'package-archives '("marmalade" . "http://marmalade-repo.org/packages/") t)
+(add-to-list 'package-archives '("melpa" . "http://melpa.milkbox.net/packages/") t)
 (package-initialize)
 
 
 (when (not package-archive-contents)
-  (package-refresh-contents))
+  (package-refesh-contents))
 
-(defvar my-packages '(js2-mode ruby-mode)
+(defvar my-packages '(js2-mode 
+		      ruby-mode 
+		      feature-mode 
+		      ghc
+		      ;; egg 
+		      ;; html-mode 
+		      ;; one-key 
+		      php-mode 
+		      ;; rhtml TODO 
+		      ;; rinari TODO 
+		      ;; ruby-test-mode TODO 
+		      rvm 
+		      sass-mode 
+		      wrap-region 
+		      yasnippet 
+		      ;; yasnippets-rails TODO
+		      zencoding-mode)
   "A list of packages to ensure are installed at launch.")
 
 (dolist (p my-packages)
